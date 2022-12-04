18a3eb81ad64f97211658ab6683606ad18c4815e
graySyntax <77895170+graySyntax@users.noreply.github.com>
2022-12-04 15:23:05 -0600

    Add files via upload

--- slapjackLogic.java
+++ slapjackLogic.java
@@ -1,17 +1,17 @@
 package slapjackLogic;
 
 import java.util.Arrays;
-
-
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
 
 public class slapjackLogic{
 
 	private int[] serverDeck;
 	
-	//score counter
-	private int player1score = 0;
-	private int player2score = 0;
+	//both players start with 5 cards
+	private int player1cards = 5;
+	private int player2cards = 5;
 	
 	//Deck on table, -1 means no cards there
 	private int[] tableDeck = {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1};
@@ -20,152 +20,103 @@ public class slapjackLogic{
 	private int[] player1deck;
 	private int[] player2deck;
 	
-	//players deck after claiming cards after a slap
-	private int[] player1deckSlap = {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1};
-	private int[] player2deckSlap = {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1};
-	
 	//increments each time a player plays, total of 5
 	private int player1turn = 0;
 	private int player2turn = 0;
 	
+	
 	public void player1JackSlap()
 	{
 		//Player 1 successfuly slaps a jack
-		System.out.println("Player 1 slaps a jack.\n");
+		System.out.println("Player 1 slaps a jack.");
 		
 		//add all cards on the table to the player's deck
 
-		//get the size of the tableDeck
-		int tableDeckSize = 0; 
-		for(int i = 0; i < 10;i++)
+		player1cards += getTableDeckSize();
+		
+		//reset the table deck, all cards gone
+		for(int i = 0; i < 10; i++)
 		{
-			if(tableDeck[i] != -1)
-			{
-				++tableDeckSize;
-			}
+		tableDeck[i] = -1;
 		}
-		
-		//get the size of the tableDeck
-		int player1deckSlapSize = 0; 
-				for(int i = 0; i < 10;i++)
-				{
-					if(player1deckSlap[i] != -1)
-					{
-						++player1deckSlapSize;
-					}
-				}
-				
-		         
-				int x = 0;
-				for(int i = player1deckSlapSize; i < 10; i++)
-				{
-					if(player1deckSlap[i] == -1)
-					{   //give player 1 the card on the table
-						player1deckSlap[i] = tableDeck[x];
-						
-					
-						//take away that same card from the table deck
-						tableDeck[x] = -1;
-						
-						//increment for the table deck
-						++x;
-					}
-				}
-
-		
-		//Display the player 1 new deck 
-		System.out.println("Player1deckSlap: " + Arrays.toString(this.player1deckSlap));
-		
-		//Display the table deck after player 1 slap
-		System.out.println("Table deck: " + Arrays.toString(this.tableDeck));
 		
 	}
 	
+	//add all cards on the table, to the players score count
 	public void player2JackSlap()
 	{
 		//Player 2 successfuly slaps a jack
 		System.out.println("Player 2 slaps a jack.\n");
-				
 		
 		//add all cards on the table to the player's deck
-
-		//get the size of the tableDeck
-		int tableDeckSize = 0; 
-		for(int i = 0; i < 10;i++)
+		player2cards += getTableDeckSize();
+		
+		//reset the table deck, all cards gone
+		for(int i = 0; i < 10; i++)
 		{
-			if(tableDeck[i] != -1)
-			{
-				++tableDeckSize;
-			}
+		tableDeck[i] = -1;
 		}
 		
-		//get the size of the tableDeck
-		int player2deckSlapSize = 0; 
-				for(int i = 0; i < 10;i++)
-				{
-					if(player2deckSlap[i] != -1)
-					{
-						++player2deckSlapSize;
-					}
-				}
-				
-		         
-				int x = 0;
-				for(int i = player2deckSlapSize; i < 10; i++)
-				{
-					if(player2deckSlap[i] == -1)
-					{   //give player 1 the card on the table
-						player2deckSlap[i] = tableDeck[x];
-						
-					
-						//take away that same card from the table deck
-						tableDeck[x] = -1;
-						
-						//increment for the table deck
-						++x;
-					}
-				}
+	}
 
+	
+	//get the size of the tableDeck
+	public int getTableDeckSize()
+	{
 		
-		//Display the player 1 new deck 
-		System.out.println("Player2deckSlap: " + Arrays.toString(this.player2deckSlap));
-		
-		//Display the table deck after player 1 slap
-		System.out.println("Table deck: " + Arrays.toString(this.tableDeck));
-		
+		int tableDeckSize = 0; 
+		for(int i = 0; i < 10;i++)
+			{
+				if(tableDeck[i] != -1)
+				{
+					++tableDeckSize;
+					}
+				}
+		return tableDeckSize;
 	}
 	
 	//player 1 turn, places card on table
-	public void player1play()
+	public void player1turn()
 	{
 		//add the card to the table deck
 		addCardToTableDeck(this.player1deck[player1turn]);
+		
+		System.out.println("Player 1 card added to table deck. Deck: " + Arrays.toString(this.tableDeck));
 		
 		//take that card out of the player's deck
 		player1deck[player1turn] = -1;
 		
 		//increment number of turns
 		++player1turn;
+		
+		//player plays a card
+		--player1cards;
 	}
 	
 	
 	//player 1 turn, places card on table
-	public void player2play()
+	public void player2turn()
 	{
 		//add the card to the table deck
 		addCardToTableDeck(this.player2deck[player2turn]);
 				
+		System.out.println("Player 2 card added to table deck. Deck: " + Arrays.toString(this.tableDeck)+"\n");
+	
 		//take that card out of the player's deck
 		player2deck[player2turn] = -1;
 		
 		//increment number of turns
 		++player2turn;
+		
+		//player plays a card
+		--player2cards;
 	}
 	
 	
 	//function to add a card to the table deck
 	public void addCardToTableDeck(int card)
 	{
+		
 		int i = 0; 
 		while(true)
 		{
@@ -177,7 +128,6 @@ public class slapjackLogic{
 			++i;
 		}
 		
-		System.out.println("Card added to table deck. Deck: " + Arrays.toString(this.tableDeck));
 	}
 	
 	
@@ -189,16 +139,16 @@ public class slapjackLogic{
 		
 
 		//randomize the deck
-		Random rgen = new Random();  // Random number generator			
+		Random rng = new Random();  // Random number generator			
 		
 				for (int i=0; i<deck.length; i++) {
-				    int randomPosition = rgen.nextInt(deck.length);
+				    int randomPosition = rng.nextInt(deck.length);
 				    int temp = deck[i];
 				    deck[i] = deck[randomPosition];
 				    deck[randomPosition] = temp;
 				}
 		
-		System.out.println("Deck:" + Arrays.toString(deck)+"\n");
+		System.out.println("Server deck:" + Arrays.toString(deck)+"\n");
 		
 		
 		serverDeck = deck;
@@ -235,8 +185,40 @@ public class slapjackLogic{
 		
 		//assign second half to player 2
 		player2deck = Arrays.copyOfRange(serverDeck, splitSize, serverDeck.length); //middle to max
+		player2deck[4] = 0;
+		
 		System.out.println("Player 2 deck: " + Arrays.toString(player2deck)+"\n");
 				
+		return player2deck;
+	}
+	
+	public int getPlayer1Cards()
+	{
+		System.out.println("Player 1 cards: " + player1cards);
+		return player1cards;
+	}
+	
+	public int getPlayer2Cards()
+	{
+		System.out.println("Player 2 cards: " + player2cards);
+		return player2cards;
+	}
+	
+	public int[] getTableDeck()
+	{
+		System.out.println("Table deck: " + Arrays.toString(tableDeck)+"\n");
+		return tableDeck;
+	}
+	
+	public int[] getPlayer1Deck()
+	{
+		System.out.println("Player 1 deck: " + Arrays.toString(player1deck)+"\n");
+		return player1deck;
+	}
+	
+	public int[] getPlayer2Deck()
+	{
+		System.out.println("Player 2 deck: " + Arrays.toString(player2deck)+"\n");
 		return player2deck;
 	}
 	
@@ -253,20 +235,42 @@ public class slapjackLogic{
 		
 		System.out.println("------------------------------------------------------");
 		
-		//loop to run 5 times, there are only 5 rounds
-		System.out.println("Each player takes a turn, table deck gets cards:");
-		for(int i = 0; i <= 4; i++)
-		{
-			gamePanel.player1play();
-			gamePanel.player2play();
-		}
+
+			
+			//show starting hand
+			gamePanel.getPlayer1Deck();
+			gamePanel.getPlayer2Deck();
+			
+			//round 1
+			gamePanel.player1turn();
+			gamePanel.player2turn();
 		
-		System.out.println("\n");
+			//round 2
+			gamePanel.player1turn();
+			gamePanel.player2turn();
+			
+			//round 3
+			gamePanel.player1turn();
+			gamePanel.player2turn();
+			
+			gamePanel.player1JackSlap();
+			
+			//round 4
+			gamePanel.player1turn();
+			gamePanel.player2turn();
+			
+			gamePanel.player2JackSlap();
+			
+			//round 5
+			gamePanel.player1turn();
+			gamePanel.player2JackSlap();
+			
+			//show the total cards
+			gamePanel.getPlayer1Cards();
+			gamePanel.getPlayer2Cards();
+			
+			System.out.println("------------------------------------------------------");
 		
-		System.out.println("------------------------------------------------------");
-		
-		//player 1 slaps a jack, gets the cards all on table
-		gamePanel.player1JackSlap();
 	}
 	
 
